7773b0d59c24688e16e858e1e8b71f03ef5f8675
owlman <savva-fokin2002@mail.ru>
2023-11-06 19:22:52 +0300

    bomb-baby added

---
+++ bomb-baby.py
@@ -0,0 +1,27 @@
+def solution(m, f):
+    m = int(m)
+    f = int(f)
+    ct = 0
+    # print(m, f, ct)
+    while m != 1 or f != 1:
+        if (m == f or m % f == 0 or f % m == 0) and m != 1 and f != 1:
+            return 'impossible'
+        elif m > f:
+            if m // f > 1 and m % f != 0:
+                ct += m // f
+                m = m % f
+            else:
+                m = m - f
+                ct += 1
+        else:
+            if f // m > 1 and f % m != 0:
+                ct += f // m
+                f = f % m
+            else:
+                f = f - m
+                ct += 1
+        # print(m, f, ct)
+    return str(ct)
+
+
+print(solution('4', '7'))
